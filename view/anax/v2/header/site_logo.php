30fdf2f68b000647822fd0ba6f744ab87f321022
Oscar Lang <oscar.lang98@gmail.com>
2018-11-13 15:09:09 +0000

    fixed css for redovisa

--- view/anax/v2/header/site_logo.php
+++ view/anax/v2/header/site_logo.php
@@ -9,8 +9,8 @@ namespace Anax\View;
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
 
-
-
-?><span class="site-logo" >
-    <img src="<?= asset($siteLogo) ?>" alt="<?= $siteLogoAlt ?>">
-</span>
+/**
+*?><span class="site-logo" >
+*    <img src="<?= asset($siteLogo) ?>" alt="<?= $siteLogoAlt ?>">
+*</span>
+*/
